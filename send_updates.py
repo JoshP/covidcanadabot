7a44cd73fd57df09812efbf2358c237937fb604e
Josh Portner <joshua.p@gmail.com>
2021-01-23 16:35:27 -0800

    use reports api instead of summary

--- send_updates.py
+++ send_updates.py
@@ -6,10 +6,6 @@
 import logging
 import requests
 import os
-import pytz
-
-from uuid import uuid4
-from datetime import datetime, timezone
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
@@ -21,27 +17,25 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-last_message_sent="2021-01-22"
+# Keeps track of dates we have processed and sent
+dates_sent = {"2021-01-21", "2021-01-22"}
 
 def get_covid_info(province):
-    url = 'https://api.covid19tracker.ca/summary/split'
-    response = requests.get(url)
+    after = {'after', '2021-01-21'}
+    url = 'https://api.covid19tracker.ca/reports/province/' + province
+    response = requests.get(url, params=after)
     jsonResponse = response.json()
-    data = jsonResponse["data"]
-    for province_data in data:
-        if (province_data["province"] == province):
-            return province_data
+    return jsonResponse["data"]
 
-def has_data_been_sent(today):
-    return (last_message_sent == today)
+def has_data_been_sent(date):
+    return date in dates_sent
     
 def check_new_updates(context: CallbackContext):
     province_data = get_covid_info("BC")
-    today = datetime.now(pytz.timezone('US/Pacific')).strftime("%Y-%m-%d")
-    if (province_data["date"] == today and province_data["change_cases"] != None):
-        logger.info("Change cases updated: %s", province_data["change_cases"])
-        sent_already = has_data_been_sent(today)
-        if (not sent_already):
+    for date_data in province_data:
+        # change_cases will be null if the API has no data for that date yet
+        if (not has_data_been_sent(province_data["date"]) and province_data["change_cases"] != None):
+            logger.info("Change cases updated: %s", province_data["change_cases"])
             send_update(context, province_data)
 
 def send_update(context: CallbackContext, province_data):
@@ -51,9 +45,9 @@ def send_update(context: CallbackContext, province_data):
 {province_data["change_vaccinations"]} new vaccinations.
 """
     logger.info(info)
-    context.bot.send_message(chat_id=os.getenv('CHANNEL_ID'), text=info)
-    global last_message_sent
-    last_message_sent=province_data["date"]
+    # context.bot.send_message(chat_id=os.getenv('CHANNEL_ID'), text=info)
+    global dates_sent
+    dates_sent.add(province_data["date"])
 
 def main() -> None:
     token = os.getenv('TOKEN');
