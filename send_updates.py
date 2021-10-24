2d26dbe5e4c2453dfc681830a4ba53b65d4c6990
Josh Portner <joshua.p@gmail.com>
2021-10-24 11:13:59 -0700

    emojis

--- send_updates.py
+++ send_updates.py
@@ -49,13 +49,11 @@ def send_update(context: CallbackContext, date_data):
     user_list = ['Dave', 'Erik', 'Graham', 'Josh', 'Peter']
     user_of_the_day = random.choice(user_list)
     info = f"""{province} - {date_data["date"]} update:
-{date_data["change_cases"]} new cases,
-{date_data["change_fatalities"]} new fatalities,
-{date_data["change_vaccinations"]} new vaccinations,
-{date_data["change_boosters_1"]} new boosters,
+😷 {date_data["change_cases"]} new cases, 🪦 {date_data["change_fatalities"]} new fatalities,
+💉 {date_data["change_vaccinations"]} new vaccinations, {date_data["change_boosters_1"]} new boosters,
 {date_data["change_vaccinated"]} people newly vaccinated,
-{date_data["total_vaccinated"]} total vaccinated, {round((date_data["total_vaccinated"]*100.0)/4634349, 2)}% of eligible people.
-User of the day is {user_of_the_day}.
+{date_data["total_vaccinated"]} total vaccinated, {round((date_data["total_vaccinated"]*100.00)/4634349, 2)}% of eligible people.
+🏆 User of the day is {user_of_the_day}.
 """
     logger.info(info)
     if (LIVE_MODE):
