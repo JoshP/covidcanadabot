6c26ce7946c1d7ae33896d3bd36f2190abcba5c6
Josh Portner <joshua.p@gmail.com>
2021-10-26 09:36:33 -0700

    formatting

--- send_updates.py
+++ send_updates.py
@@ -49,11 +49,14 @@ def send_update(context: CallbackContext, date_data):
     user_list = ['Dave', 'Erik', 'Graham', 'Josh', 'Peter']
     user_of_the_day = random.choice(user_list)
     info = f"""{province} - {date_data["date"]} update:
-😷 {date_data["change_cases"]} new cases, 🪦 {date_data["change_fatalities"]} new fatalities,
-💉 {date_data["change_vaccinations"]} new vaccinations, {date_data["change_boosters_1"]} new boosters,
-{date_data["change_vaccinated"]} people newly vaccinated,
-{date_data["total_vaccinated"]} total vaccinated, {round((date_data["total_vaccinated"]*100.00)/4634349, 2)}% of eligible people.
-🏆 User of the day is {user_of_the_day}.
+😷 {date_data["change_cases"]} new cases, 
+🪦 {date_data["change_fatalities"]} new fatalities,
+💉 {date_data["change_vaccinations"]} new vaxx,
+🚀 {date_data["change_boosters_1"]} new boosters,
+{date_data["change_vaccinated"]} newly vaxxed,
+{date_data["total_vaccinated"]} total vaxxed,
+{round((date_data["total_vaccinated"]*100.00)/4634349, 2)}% vaxxed of eligible.
+🏆 User of the day is {user_of_the_day}!
 """
     logger.info(info)
     if (LIVE_MODE):
